fef5964af22c0dc1e766011a393156ce968e13b0
root <root@rodrigo-HP-1000-Notebook-PC>
2020-08-12 07:48:12 +0000

    del cierre convexo se obtienen las tangentes y sus triangulos minimales

--- JavaGraphics/src/triangulomaximal/Poligono.java
+++ JavaGraphics/src/triangulomaximal/Poligono.java
@@ -18,17 +18,18 @@ public class Poligono {
 
     int cardinalidad;
     PuntoPoligono ultimoInsertado;
+    PuntoPoligono referencia = new PuntoPoligono(0, 0);
+    PuntoPoligono tangente;
+    PuntoPoligono inicioBusqueda;
 
     public static void main(String[] args) {
         Poligono poli = new Poligono();
         ArrayList<PuntoPoligono> puntos = poli.crearOrden();
         poli.CrearArregloConvexo(puntos);
-        PuntoPoligono x = poli.ultimoInsertado;
-        for (int i = 0; i < 10; i++) {
-            System.out.println("" + x.x + "  " + x.y);
-            x = x.next.peek();
-            //x = x.prev;
-        }
+        poli.recorerConvexo(poli.ultimoInsertado);
+        poli.reverseOrder(puntos);
+        double angle = poli.calcDegrees(poli.ultimoInsertado, poli.ultimoInsertado.next.peek());
+        System.out.println(angle);
     }
 
     public void CrearArregloConvexo(ArrayList<PuntoPoligono> puntos) {
@@ -56,7 +57,7 @@ public class Poligono {
     public void agregarPunto(PuntoPoligono p) {
         PuntoPoligono prevAux;
         PuntoPoligono nextAux;
-        
+
         prevAux = buscarPrev(p);
         nextAux = buscarNext(p);
 
@@ -106,6 +107,103 @@ public class Poligono {
             c = c.next.peek();
         }
         return b;
+    }
+
+    public void recorerConvexo(PuntoPoligono p) {
+        PuntoPoligono iterador;
+        PuntoPoligono minY;
+        iterador = p;
+        minY = iterador;
+        do {
+            System.out.println("convexo" + iterador.x + " " + iterador.y);
+            if (iterador.y < minY.y) {
+                minY = iterador;
+            }
+            iterador = iterador.next.peek();
+        } while (iterador != p);
+        System.out.println("menor es " + minY.x + " " + minY.y);
+        inicioBusqueda = minY;
+    }
+
+    public void reverseOrder(ArrayList<PuntoPoligono> puntos) {
+        PuntoPoligono p;
+        for (int i = puntos.size() - 1; i >= 0; i--) {
+            p = puntos.get(i);
+            if (p.color.equals(Color.blue)) {
+                //eliminar punto
+                eliminarPunto(p);
+            } else if (p.color.equals(Color.red)) {
+
+                if (cardinalidad == 1) {
+                    tangente = inicioBusqueda;
+                    System.out.println("rojo " + p.x + " " + p.y);
+                    System.out.println("tangente " + tangente.x + " " + tangente.y);
+                    System.out.println("area " + (area2(referencia, p, tangente)) / 2);
+                } else if (cardinalidad == 0) {
+
+                } else {
+                    buscarTangente(p);
+                    System.out.println("rojo " + p.x + " " + p.y);
+                    System.out.println("tangente " + tangente.x + " " + tangente.y);
+                    System.out.println("area " + (area2(referencia, p, tangente)) / 2);
+                }
+
+            }
+        }
+    }
+
+    public void buscarTangente(PuntoPoligono rojo) {
+        PuntoPoligono tangenteAux;
+        tangenteAux = inicioBusqueda;
+
+        while (calcDegrees(tangenteAux, tangenteAux.next.peek()) < calcDegrees(referencia, rojo)) {
+            tangenteAux = tangenteAux.next.peek();
+        }
+        inicioBusqueda = tangenteAux;
+        tangente = tangenteAux;
+        return;
+        //return ;
+
+    }
+
+    public void eliminarPunto(PuntoPoligono p) {
+        if (p == inicioBusqueda) {
+            inicioBusqueda = p.prev;
+        } else {
+            inicioBusqueda = inicioBusqueda; //asignacion para clarificar
+        }
+        cardinalidad = cardinalidad - 1;
+        p.prev.next.pop();
+    }
+
+    public double calcDegrees(PuntoPoligono centerPt, PuntoPoligono targetPt) {
+        // calculate the angle theta from the deltaY and deltaX values
+        // (atan2 returns radians values from [-PI,PI])
+        // 0 currently points EAST.  
+        // NOTE: By preserving Y and X param order to atan2,  we are expecting 
+        // a CLOCKWISE angle direction.  
+        double theta = Math.atan2(targetPt.y - centerPt.y, targetPt.x - centerPt.x);
+
+        // rotate the theta angle clockwise by 90 degrees 
+        // (this makes 0 point NORTH)
+        // NOTE: adding to an angle rotates it clockwise.  
+        // subtracting would rotate it counter-clockwise
+        //theta += (Math.PI/2.0)*2;
+        theta += 0;
+
+        // convert from radians to degrees
+        // this will give you an angle from [0->270],[-180,0]
+        double angle = Math.toDegrees(theta);
+
+        // convert to positive range [0-360)
+        // since we want to prevent negative angles, adjust them now.
+        // we can assume that atan2 will not return a negative value
+        // greater than one partial rotation
+        if (angle < 0) {
+            angle += 360;
+        }
+
+        return angle;
     }
 
     public ArrayList crearOrden() {
